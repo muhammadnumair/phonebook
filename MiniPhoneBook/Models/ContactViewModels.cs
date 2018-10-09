9c04f419ca4f456dda808e5aeb3965caf75f4e4b
Summen Zahid <summenzahid@gmail.com>
2018-10-09 18:11:20 +0500

    Contact View Model

---
+++ MiniPhoneBook/Models/ContactViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiniPhoneBook.Models
+{
+    public class ContactViewModels
+    {
+        public int ContactId { get; set; }
+
+        [Required]
+        [RegularExpression("[0-9]*", ErrorMessage = "Please Enter Valid Contact Number ")]
+        [Display(Name = "Contact #")]
+        public string ContactNumber { get; set; }
+
+        [RegularExpression("[A-Za-z]*", ErrorMessage = "Please Enter Valid Type")]
+        [Required]
+        public string Type { get; set; }
+        public int PersonId { get; set; }
+
+    }
+}
